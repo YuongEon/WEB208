cb441ca63cce10e579f7247aa894dbdfe28e849a
YuongEon <boyjackgamer@gmail.com>
2023-05-12 23:05:28 +0700

    lab2 rework

--- lab2/src/app/app.module.ts
+++ lab2/src/app/app.module.ts
@@ -5,16 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { BannerComponent } from './components/banner/banner.component';
-import { AlbumComponent } from './components/album/album.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
-    BannerComponent,
-    AlbumComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,
